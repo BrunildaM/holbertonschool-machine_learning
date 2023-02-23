20d8e51e19f6e18417360cc85e3db8a86a391ad0
BrunildaM <brunildamuco6@gmail.com>
2023-02-23 14:56:11 -0800

    add: 5-across_the_planes.py

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""a function that adds two matrices element-wise"""
+
+
+def add_matrices2D(mat1, mat2):
+    """a function that adds two matrices element-wise"""
+    mat = []
+    if len(mat1[0]) != len(mat2[0]) or len(mat1) != len(mat2):
+        return None
+    for i in range(len(mat1)):
+        row = []
+        for j in range(len(mat1[0])):
+            row.append(mat1[i][j] + mat2[i][j])
+        mat.append(row)
+    return mat
