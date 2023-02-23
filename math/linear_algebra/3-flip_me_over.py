17b9b4abf66d87576f4585b92c8a3ada2e9bda7c
BrunildaM <brunildamuco6@gmail.com>
2023-02-23 03:14:57 -0800

    added: 3-flip_me_over.py

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -4,9 +4,9 @@
 
 def matrix_transpose(matrix):
     """a function that returns the transpose of a 2D matrix"""
-    transpose=[]
+    transpose = []
     for i in range(len(matrix[0])):
-        row=[]
+        row = []
         for j in range(len(matrix)):
             row.append(matrix[j][i])
         transpose.append(row)
