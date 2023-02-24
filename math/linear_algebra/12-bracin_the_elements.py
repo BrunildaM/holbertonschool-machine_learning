6472a401bfb59160b4c5e04bf55d55659c6f4d17
BrunildaM <brunildamuco6@gmail.com>
2023-02-24 01:11:49 -0800

    :add, sub, mul, div matrices

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -8,4 +8,4 @@ def np_elementwise(mat1, mat2):
     sub = np.subtract(mat1, mat2)
     mul = np.multiply(mat1, mat2)
     div = np.divide(mat1, mat2)
-    return add, sub, mul, div
+    return [add, sub, mul, div]
