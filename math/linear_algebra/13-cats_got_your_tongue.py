7558a4b29214956b031c08ad9f357c088f271f12
BrunildaM <brunildamuco6@gmail.com>
2023-02-24 01:55:52 -0800

    concatenate

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
+import numpy as np
 """function concatenate that concatenates two matrices along a specific axis"""
 
 
-import numpy as np
 def np_cat(mat1, mat2, axis=0):
     """function concate that concatenates two matrices along a specific axis"""
     mat = np.concatenate((mat1, mat2), axis)
