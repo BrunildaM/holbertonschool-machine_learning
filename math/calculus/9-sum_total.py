d0f589b1b4c7cbc0e405c05efea4c642ce3ec9c6
BrunildaM <brunildamuco6@gmail.com>
2023-03-02 07:22:07 -0800

    Sum of Squares

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -4,6 +4,6 @@
 
 def summation_i_squared(n):
     """a function that calculates the sum of squared numbers"""
-    if type(n) != int:
+    if isinstance(n, int):
         return None
     return int((n*(n+1)*(2*n+1))/6)
