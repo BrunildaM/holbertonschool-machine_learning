388786901ef60e588c5393cf3ed8f0eaff33fa95
BrunildaM <brunildamuco6@gmail.com>
2023-03-20 14:53:33 -0700

    pooling

--- math/convolutions_and_pooling/6-pool.py
+++ math/convolutions_and_pooling/6-pool.py
@@ -1,27 +1,22 @@
 #!/usr/bin/env python3
-"""A function for Convolution with pooling"""
+"""Convolution with pooling"""
 import numpy as np
 
 
 def pool(images, kernel_shape, stride, mode='max'):
-    """A function for Convolution with pooling"""
+    """Convolution with pooling"""
     m, h, w, c = images.shape
     kh, kw = kernel_shape
     sh, sw = stride
+    out_h = int((h - kh) / sh + 1)
+    out_w = int((w - kw) / sw + 1)
+    out = np.zeros((m, out_h, out_w, c))
 
-    pool_h = ((h - kh) // sh) + 1
-    pool_w = ((w - kw)  // sw) + 1
-    pooled = np.zeros((m, pool_h, pool_w, c))
-
-    i  = 0
-    for h in range(0, h - kh + 1, sh):
-        j = 0
-        for w in range(0, w - kw + 1, sw):
-            if mode == 'max':
-                output = np.max(images[:, h: h +kh, w: w +kw, :], axis = (1, 2))
-                if mode == 'avg':
-                    output = np.average(images[:, h: h +kh, w: w +kw, :], axis = (1, 2))
-                    pooled[:, i, j, :] = output
-                    j += 1
-                i += 1
-    return pooled
+    for i in range(out_h):
+        for j in range(out_w):
+            if mode == 'avg':
+                window = np.mean(images[:, i * sh: i * sh + kh, j * sw: j * sw + kw, :], axis=(1, 2))
+            else:
+                window = np.max(images[:, i * sh: i * sh + kh, j * sw: j * sw + kw, :], axis=(1, 2))
+            out[:, i, j, :] = window
+    return out
