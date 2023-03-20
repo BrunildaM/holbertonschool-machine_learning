cd98565e210e20d93a43ccef5f6b8ff862ab51c9
BrunildaM <brunildamuco6@gmail.com>
2023-03-20 16:12:13 -0700

    exponential

---
+++ math/probability/exponential.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""defines Exponential class that represents a exponential distribution"""
+
+
+class Exponential:
+    """a class that represents a exponential distribution"""
+    def __init__(self, data=None, lambtha=1.):
+        self.lambtha = float(lambtha)
+        if self.lambtha <= 0:
+            raise ValueError("lambtha must be a positive value")
+        if data is not None:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = float(len(data)) / sum(data)
+
+    def pdf(self, x):
+        """calculates the value of the PDF for a given time period"""
+        if (x < 0):
+            return 0
+        e = 2.7182818285
+        lambtha = self.lambtha
+        pdf = lambtha * (e ** (-lambtha * x))
+        return pdf
+
+    def cdf(self, x):
+        """calculates the value of the CDF for a given time period"""
+        if (x < 0):
+            return 0
+        e = 2.7182818285
+        lambtha = self.lambtha
+        cdf = 1 - e ** (-lambtha * x)
+        return cdf
