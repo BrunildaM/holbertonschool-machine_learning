0cdff217239bf3197982843269c93409bd7f9734
BrunildaM <brunildamuco6@gmail.com>
2023-03-20 14:45:42 -0700

    pooling

--- math/convolutions_and_pooling/6-pool.py
+++ math/convolutions_and_pooling/6-pool.py
@@ -5,18 +5,18 @@ import numpy as np
 
 def pool(images, kernel_shape, stride, mode='max'):
     """A function for Convolution with pooling"""
-    m, height, width, c = images.shape
+    m, h, w, c = images.shape
     kh, kw = kernel_shape
     sh, sw = stride
 
-    pool_h = ((height - kh) // sh) + 1
-    pool_w = ((width - kw)  // sw) + 1
+    pool_h = ((h - kh) // sh) + 1
+    pool_w = ((w - kw)  // sw) + 1
     pooled = np.zeros((m, pool_h, pool_w, c))
 
     i  = 0
-    for h in range(0, height - kh + 1, sh):
+    for h in range(0, h - kh + 1, sh):
         j = 0
-        for w in range(0, width - kw + 1, sw):
+        for w in range(0, w - kw + 1, sw):
             if mode == 'max':
                 output = np.max(images[:, h: h +kh, w: w +kw, :], axis = (1, 2))
                 if mode == 'avg':
