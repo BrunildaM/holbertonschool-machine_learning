bc32c1b8a213965593dc23dc73ca2356466725d6
BrunildaM <brunildamuco6@gmail.com>
2023-03-20 10:14:50 -0700

    changes

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -1,16 +1,16 @@
+
 #!/usr/bin/env python3
-"""Performs a same convolution on grayscale images"""
-
-
 import numpy as np
+
 """Performs a same convolution on grayscale images"""
 
 
 def convolve_grayscale_same(images, kernel):
-    """Performs a same convolution on grayscale images"""
+    """
+    Performs a valid convolution on grayscale images
+    """
     m, height, width = images.shape
     kh, kw = kernel.shape
-
     if (kh % 2) == 1 and (kw % 2) == 1:
         ph = (kh - 1) // 2
         pw = (kw - 1) // 2
@@ -18,13 +18,13 @@ def convolve_grayscale_same(images, kernel):
         ph = kh // 2
         pw = kw // 2
 
-    images = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), 'constant',\
-                    constant_values = 0)
-    convolted = np.zeros((m, height, width))
-    
-    for h in range(height - kh + 1):
-        for w in range(width - kw + 1):
-            out = np.sum(images[:, h: h + kh, w: w + kw]\
-                         * kernel, axis = 1).sum(axis = 1)
+
+    images = np.pad(images, ((0,0), (ph,ph), (pw,pw)), 'constant')
+    convoluted = np.zeros((m, height, width))
+
+    for h in range(height):
+        for w in range(width):
+            output = np.sum(images[:, h: h + kh, w: w + kw] * kernel, axis = 1)\
+                       .sum(axis = 1)
             convoluted[:, h, w] = output
     return convoluted
