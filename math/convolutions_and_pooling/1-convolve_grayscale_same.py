5ad1444fbe6144a834ef959a00963b326f02902e
BrunildaM <brunildamuco6@gmail.com>
2023-03-20 10:21:39 -0700

    changes

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -20,7 +20,7 @@ def convolve_grayscale_same(images, kernel):
     convoluted = np.zeros((m, height, width))
     for h in range(height):
         for w in range(width):
-            output = np.sum(images[:, h: h + kh, w: w + kw] * kernel, axis = 1)\
-                       .sum(axis = 1)
+            output = np.sum(images[:, h: h + kh, w: w + kw] * kernel, axis=1)\
+                       .sum(axis=1)
             convoluted[:, h, w] = output
     return convoluted
