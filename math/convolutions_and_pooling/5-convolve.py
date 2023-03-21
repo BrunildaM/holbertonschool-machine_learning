dd0b4f08b14a6398c5c36eb4a78cd8ebf4933409
BrunildaM <brunildamuco6@gmail.com>
2023-03-21 13:10:56 -0700

    done

--- math/convolutions_and_pooling/5-convolve.py
+++ math/convolutions_and_pooling/5-convolve.py
@@ -1,28 +1,35 @@
 #!/usr/bin/env python3
-"""Convolution with multiple kernels"""
+"""Performs a convolution on images with nc kernels"""
 import numpy as np
 
 
-def convolve(images, kernels, padding='same', stride=(1, 1)):
-    """Convolution with multiple kernels"""
-    m, h, w, c = images.shape
-    kh, kw, _, nc = kernels.shape
+def convolve(images, kernels, padding="same", stride=(1, 1)):
+    """Performs a convolution on images with nc kernels"""
+    m, height, width, c = images.shape
+    kh, kw, c, nc = kernels.shape
     sh, sw = stride
-    if padding == 'same':
-        ph = int(((h - 1) * sh + kh - h) / 2)
-        pw = int(((w - 1) * sw + kw - w) / 2)
-    elif padding == 'valid':
-        ph = pw = 0
+
+    if padding == "same":
+        ph = ((height - 1) * sh + kh - height) // 2 + 1
+        pw = ((width - 1) * sw + kw - width) // 2 + 1
+    elif padding == "valid":
+        ph = 0
+        pw = 0
     else:
         ph, pw = padding
-    imp = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)), mode='constant')
-    out_h = int((h + 2 * ph - kh) / sh + 1)
-    out_w = int((w + 2 * pw - kw) / sw + 1)
-    out_5 = np.zeros((m, out_h, out_w, nc))
-    for k in range(nc):
-        for i in range(out_h):
-            for j in range(out_w):
-                window = imp[:, i*sh:i*sh+kh, j*sw:j*sw+kw, :]
-                out_5[:, i, j, k] = np.sum(window * kernels[..., k],
-                                           axis=(1, 2, 3))
-    return out_5
+
+    p_images = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)))
+
+    ch = ((height + 2 * ph - kh) // sh) + 1
+    cw = ((width + 2 * pw - kw) // sw) + 1
+
+    convoluted = np.zeros((m, ch, cw, nc))
+
+    for h in range(ch):
+        for w in range(cw):
+            for k in range(nc):
+                output = (p_images[:, h * sh: h * sh + kh,
+                                   w * sw: w * sw + kw] * kernels[:, :, :, k])
+                sum_out = np.sum(output, axis=(1, 2, 3))
+                convoluted[:, h, w, k] = sum_out
+    return convoluted
