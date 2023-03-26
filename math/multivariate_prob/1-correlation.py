69a0d0af2d980d7e6bd3bbd59e865c7fee55b0fe
BrunildaM <brunildamuco6@gmail.com>
2023-03-26 09:37:34 -0700

    done

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -7,7 +7,7 @@ def correlation(C):
     """a function that calculates a correlation matrix"""
     if not isinstance(C, np.ndarray):
         raise TypeError("C must be a numpy.ndarray")
-    if C.shape[0] != C.shape[1]:
+    if C.ndim != 2 or C.shape[0] != C.shape[1]:
         raise ValueError("C must be a 2D square matrix")
 
     d = C.shape[0]
