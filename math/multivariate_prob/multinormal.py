6b88e6cdaf4350c3a99fbb723a265fa98f01cdfc
BrunildaM <brunildamuco6@gmail.com>
2023-03-26 10:16:51 -0700

    done

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -21,12 +21,13 @@ class MultiNormal:
         """
         public instance method def that calculates the PDF at a data point
         """
-        d = self.mean.shape[0]
         if not isinstance(x, np.ndarray):
             raise TypeError("x must be a numpy.ndarray")
-        if x.shape != self.mean.shape:
-            raise ValueError("x must have the shape ({}, 1)".format(d)
-                             
+        if x.shape != (self.mean.shape[0], 1):
+            d = self.mean.shape[0]
+            raise ValueError("x must have the shape ({}, 1)".format(d))
+
+        d = self.mean.shape[0]                     
         diff = x - self.mean
         exponent = -0.5 * np.dot(np.dot(diff.T, np.linalg.inv(self.cov)), diff)
         prefactor = 1 / (np.sqrt((2 * np.pi) ** d * np.linalg.det(self.cov)))
