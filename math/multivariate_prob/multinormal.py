20998d8ccfaaa8537670154ba093c6905308678a
BrunildaM <brunildamuco6@gmail.com>
2023-03-26 10:19:23 -0700

    done

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -27,7 +27,7 @@ class MultiNormal:
             d = self.mean.shape[0]
             raise ValueError("x must have the shape ({}, 1)".format(d))
 
-        d = self.mean.shape[0]                     
+        d = self.mean.shape[0]
         diff = x - self.mean
         exponent = -0.5 * np.dot(np.dot(diff.T, np.linalg.inv(self.cov)), diff)
         prefactor = 1 / (np.sqrt((2 * np.pi) ** d * np.linalg.det(self.cov)))
