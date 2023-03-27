db5c04709cea9959bf73224ec725f56604cee61f
BrunildaM <brunildamuco6@gmail.com>
2023-03-27 01:17:34 -0700

    likelihood

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -8,7 +8,9 @@ def likelihood(x, n, P):
     if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
     if not isinstance(x, int) or x < 0:
-        raise ValueError("x must be an integer that is greater than or equal to 0")
+        raise ValueError("""
+                         x must be an integer that is greater than or equal to 0
+                         """)
     if x > n:
         raise ValueError("x cannot be greater than n")
     if not isinstance(P, np.ndarray) or P.ndim != 1:
@@ -16,6 +18,8 @@ def likelihood(x, n, P):
     if np.any(P < 0) or np.any(P > 1):
         raise ValueError("All values in P must be in the range [0, 1]")
 
-    likelihoods = np.array([np.math.comb(n, x) * (p**x) * ((1-p)**(n-x)) for p in P])
-
+    terms = np.arange(1, k+1, dtype=np.float)
+    bc = int(np.prod((n - terms + 1) / terms))
+    likelihoods = np.array([bc * (p**x) * ((1-p)**(n-x)) for p in P])
+    
     return likelihoods
