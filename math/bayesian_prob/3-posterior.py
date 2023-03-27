57299ec61a5d71783a643acac7febb1a182a7d9f
BrunildaM <brunildamuco6@gmail.com>
2023-03-27 10:00:11 -0700

    done

--- math/bayesian_prob/3-posterior.py
+++ math/bayesian_prob/3-posterior.py
@@ -8,12 +8,14 @@ def posterior(x, n, P, Pr):
     if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
     if not isinstance(x, int) or x < 0:
-        raise ValueError("x must be an integer that is greater than or equal to 0")
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
     if not isinstance(P, np.ndarray) or len(P.shape) != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
-    if not isinstance(Pr, np.ndarray) or len(Pr.shape) != 1 or Pr.shape != P.shape:
+    if not isinstance(Pr, np.ndarray) or len(Pr.shape) != 1 or \
+       Pr.shape != P.shape:
         raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
     if np.any(P < 0) or np.any(P > 1):
         raise ValueError("All values in P must be in the range [0, 1]")
