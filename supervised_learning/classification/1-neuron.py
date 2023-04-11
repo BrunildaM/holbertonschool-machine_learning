3a275a4f1ee000e3b7035c12f125025a1ac58a61
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-04-11 09:03:10 +0200

    Update 1-neuron.py

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -13,18 +13,18 @@ class Neuron:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        self.__W = np.random.normal(size=(nx,))
+        self.__W = np.random.randn(1, nx)
         self.__b = 0
         self.__A = 0
         
     @property
     def W(self):
         return (self.__W)
-
+    
     @property
     def b(self):
         return (self.__b)
- 
-   @property
+    
+    @property
     def A(self):
         return (self.__A)
