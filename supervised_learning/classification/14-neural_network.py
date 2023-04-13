501df4ff7f0fca5f853af150af8764bfbe6d618d
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-04-13 12:39:43 +0200

    Update 14-neural_network.py

--- supervised_learning/classification/14-neural_network.py
+++ supervised_learning/classification/14-neural_network.py
@@ -24,11 +24,11 @@ class NeuralNetwork:
             raise TypeError("nodes must be an integer")
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
-        
+
         self.__W1 = np.random.randn(nodes, nx)
         self.__b1 = np.zeros((nodes, 1))
         self.__A1 = 0
-        
+
         self.__W2 = np.random.randn(1, nodes)
         self.__b2 = 0
         self.__A2 = 0
@@ -56,20 +56,20 @@ class NeuralNetwork:
     @property
     def A2(self):
         return (self.__A2)
-    
+
     def forward_prop(self, X):
         """
         calculates the forward propagation of the neural network
         """
-        
+
         z1 = np.matmul(self.W1, X) + self.b1
         self.__A1 = 1 / (1 + (np.exp(-z1)))
-        
+
         z2 = np.matmul(self.W2, self.__A1) + self.b2
         self.__A2 = 1 / (1 + (np.exp(-z2)))
-        
+
         return (self.A1, self.A2)
-    
+
     def cost(self, Y, A):
         """
         calculates the cost of the model using logistic regression
@@ -78,7 +78,7 @@ class NeuralNetwork:
         m_loss = np.sum((Y * np.log(A)) + ((1 - Y) * np.log(1.0000001 - A)))
         cost = (1 / m) * (-(m_loss))
         return (cost)
-    
+
     def evaluate(self, X, Y):
         """
         evaluates the neural network's predictions
@@ -87,27 +87,27 @@ class NeuralNetwork:
         cost = self.cost(Y, A2)
         prediction = np.where(A2 >= 0.5, 1, 0)
         return (prediction, cost)
-    
+
     def gradient_descent(self, X, Y, A1, A2, alpha=0.05):
         """
         calculates one pass of gradient descent on the neural network
-        """ 
+        """
         m = Y.shape[1]
-        
+
         dz2 = (A2 - Y)
         d__W2 = (1 / m) * (np.matmul(dz2, A1.transpose()))
         d__b2 = (1 / m) * (np.sum(dz2, axis=1, keepdims=True))
-        
+
         dz1 = (np.matmul(self.W2.transpose(), dz2)) * (A1 * (1 - A1))
         d__W1 = (1 / m) * (np.matmul(dz1, X.transpose()))
         d__b1 = (1 / m) * (np.sum(dz1, axis=1, keepdims=True))
-        
+
         self.__W2 = self.W2 - (alpha * d__W2)
         self.__b2 = self.b2 - (alpha * d__b2)
-        
+
         self.__W1 = self.W1 - (alpha * d__W1)
         self.__b1 = self.b1 - (alpha * d__b1)
-        
+
     def train(self, X, Y, iterations=5000, alpha=0.05):
         """
         trains the neuron and updates __W1, __b1, __A1, __W2, __b2, and __A2
@@ -120,9 +120,9 @@ class NeuralNetwork:
             raise TypeError("alpha must be a float")
         if alpha <= 0:
             raise ValueError("alpha must be positive")
-            
+
         for itr in range(iterations):
             A1, A2 = self.forward_prop(X)
             self.gradient_descent(X, Y, A1, A2, alpha)
-            
+
         return (self.evaluate(X, Y))
