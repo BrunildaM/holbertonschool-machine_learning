3998b40e523fc61b436948dba68c106671655ea1
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-04-13 12:09:21 +0200

    Update 13-neural_network.py

--- supervised_learning/classification/13-neural_network.py
+++ supervised_learning/classification/13-neural_network.py
@@ -8,12 +8,12 @@ import numpy as np
 
 class NeuralNetwork:
     """
-    Class that defines a neural network with one hidden layer
+    class that represents a neural network with one hidden layer
     performing binary classification
     """
     def __init__(self, nx, nodes):
         """
-        Constructor for NeuralNetwork class
+        class constructor
         """
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
@@ -32,48 +32,72 @@ class NeuralNetwork:
         self.__b2 = 0
         self.__A2 = 0
 
+    @property
+    def W1(self):
+        return (self.__W1)
+
+    @property
+    def b1(self):
+        return (self.__b1)
+
+    @property
+    def A1(self):
+        return (self.__A1)
+
+    @property
+    def W2(self):
+        return (self.__W2)
+
+    @property
+    def b2(self):
+        return (self.__b2)
+
+    @property
+    def A2(self):
+        return (self.__A2)
+
     def forward_prop(self, X):
         """
-        Calculates the forward propagation of the neural network
+        calculates the forward propagation of the neural network
         """
-        Z1 = np.dot(self.__W1, X) + self.__b1
-        self.__A1 = 1 / (1 + np.exp(-Z1))
-        Z2 = np.dot(self.__W2, self.__A1) + self.__b2
-        self.__A2 = 1 / (1 + np.exp(-Z2))
-        return self.__A1, self.__A2
+        z1 = np.matmul(self.W1, X) + self.b1
+        self.__A1 = 1 / (1 + (np.exp(-z1))) 
+        z2 = np.matmul(self.W2, self.__A1) + self.b2
+        self.__A2 = 1 / (1 + (np.exp(-z2)))
+        return (self.A1, self.A2)
 
     def cost(self, Y, A):
         """
-        Calculates the cost of the model using logistic regression
+        calculates the cost of the model using logistic regression
         """
         m = Y.shape[1]
-        cost = -np.sum((Y*np.log(A)) + ((1-Y)*np.log(1.0000001-A))) / m
-        return cost
+        m_loss = np.sum((Y * np.log(A)) + ((1 - Y) * np.log(1.0000001 - A)))
+        cost = (1 / m) * (-(m_loss))
+        return (cost)
 
     def evaluate(self, X, Y):
         """
-        Evaluates the neural network's predictions
-        """
-        self.forward_prop(X)
-        A = np.where(self.__A2 >= 0.5, 1, 0)
-        cost = self.cost(Y, self.__A2)
-        return A, cost
-    
-    def gradient_descent(self, X, Y, alpha=0.05):
-        """
-        Calculates one pass of gradient descent on the neural network
+        evaluates the neural network's predictions
         """
         A1, A2 = self.forward_prop(X)
+        cost = self.cost(Y, A2)
+        prediction = np.where(A2 >= 0.5, 1, 0)
+        return (prediction, cost)
+
+    def gradient_descent(self, X, Y, A1, A2, alpha=0.05):
+        """
+        calculates one pass of gradient descent on the neural network
+        """
         m = Y.shape[1]
-        dZ2 = A2 - Y
-        dW2 = np.dot(dZ2, A1.T) / m
-        db2 = np.sum(dZ2, axis=1, keepdims=True) / m
+        dz2 = (A2 - Y)
+        d__W2 = (1 / m) * (np.matmul(dz2, A1.transpose()))
+        d__b2 = (1 / m) * (np.sum(dz2, axis=1, keepdims=True))
+        dz1 = (np.matmul(self.W2.transpose(), dz2)) * (A1 * (1 - A1))
+        d__W1 = (1 / m) * (np.matmul(dz1, X.transpose()))
+        d__b1 = (1 / m) * (np.sum(dz1, axis=1, keepdims=True))
 
-        dZ1 = np.dot(self.W2.T, dZ2) * A1 * (1 - A1)
-        dW1 = np.dot(dZ1, X.T) / m
-        db1 = np.sum(dZ1, axis=1, keepdims=True) / m
+        self.__W2 = self.W2 - (alpha * d__W2)
+        self.__b2 = self.b2 - (alpha * d__b2)
 
-        self.__W2 -= alpha * dW2
-        self.__b2 -= alpha * db2
-        self.__W1 -= alpha * dW1
-        self.__b1 -= alpha * db1
+        self.__W1 = self.W1 - (alpha * d__W1)
+        self.__b1 = self.b1 - (alpha * d__b1)
