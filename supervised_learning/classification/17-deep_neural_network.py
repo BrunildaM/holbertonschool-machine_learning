86ca3f57eb2ce86e6794f35274ac6d6c492e8d2b
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-04-14 14:14:07 +0200

    Update 17-deep_neural_network.py

--- supervised_learning/classification/17-deep_neural_network.py
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -4,6 +4,7 @@ Class that defines a deep neural network performing binary classification
 """
 import numpy as np
 
+
 class DeepNeuralNetwork:
     """
     Class that defines a deep neural network performing binary classification
@@ -24,15 +25,15 @@ class DeepNeuralNetwork:
         for l in range(1, self.__L + 1):
             self.__weights['W' + str(l)] = np.random.randn(layers[l-1], nx) * np.sqrt(2/nx)
             self.__weights['b' + str(l)] = np.zeros((layers[l-1], 1))
-    
+
     @property
     def L(self):
         return self.__L
-    
+
     @property
     def cache(self):
         return self.__cache
-    
+
     @property
     def weights(self):
         return self.__weights
