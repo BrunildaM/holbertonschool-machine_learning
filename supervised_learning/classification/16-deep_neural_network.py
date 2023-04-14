75f73b118f66794cb05ce070c04777478b189619
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-04-14 13:08:27 +0200

    Update 16-deep_neural_network.py

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -7,10 +7,10 @@ import numpy as np
 
 class DeepNeuralNetwork:
     """
-    a class that defines a deep neural network performing bnary classification
+    a class that defines a deep neural network performing binary classification
     """
     def __init__(self, nx, layers):
-        """Class constructor"""
+        """class constructor """
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
@@ -29,7 +29,7 @@ class DeepNeuralNetwork:
                 self.__weights['W' + str(l)] = np.random.randn(layers[0], nx) * np.sqrt(2 / nx)
             else:
                 self.__weights['W' + str(l)] = np.random.randn(layers[l - 1], layers[l - 2]) * np.sqrt(2 / layers[l - 2])
-            
+
             self.__weights['b' + str(l)] = np.zeros((layers[l - 1], 1))
 
     @property
