e30adca5afc97835c64767f46b050418b57f5458
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-04-14 13:35:48 +0200

    Update 16-deep_neural_network.py

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -7,17 +7,17 @@ import numpy as np
 
 class DeepNeuralNetwork:
     """
-    a class that defines a deep neural network performing binary classification
-    """
+    a class that defines a deep neural nework performing binary classification
+    """"
     def __init__(self, nx, layers):
-        """class constructor """
-        if not isinstance(nx, int):
+        """class conustructor"""
+        if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if not isinstance(layers, list) or len(layers) == 0:
+        if type(layers) is not list or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-        if not all(isinstance(i, int) and i > 0 for i in layers):
+        if not all(isinstance(n, int) and n > 0 for n in layers):
             raise TypeError("layers must be a list of positive integers")
 
         self.__L = len(layers)
@@ -26,10 +26,10 @@ class DeepNeuralNetwork:
 
         for l in range(1, self.__L + 1):
             if l == 1:
-                self.__weights['W' + str(l)] = np.random.randn(layers[0], nx) * np.sqrt(2 / nx)
+                self.__weights["W1"] = np.random.randn(layers[0], nx) * np.sqrt(2/nx)
             else:
-                self.__weights['W' + str(l)] = np.random.randn(layers[l - 1], layers[l - 2]) * np.sqrt(2 / layers[l - 2])
-            self.__weights['b' + str(l)] = np.zeros((layers[l - 1], 1))
+                self.__weights[f"W{l}"] = np.random.randn(layers[l-1], layers[l-2]) * np.sqrt(2/layers[l-2])
+            self.__weights[f"b{l}"] = np.zeros((layers[l-1], 1))
 
     @property
     def L(self):
