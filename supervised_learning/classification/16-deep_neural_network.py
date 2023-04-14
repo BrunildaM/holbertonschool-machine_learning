2672c444b51491e8cbc85d988256e1b85cbc2893
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-04-14 13:05:31 +0200

    Update 16-deep_neural_network.py

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -7,7 +7,7 @@ import numpy as np
 
 class DeepNeuralNetwork:
     """
-    a class that defines a deep neural network performing binary classification
+    a class that defines a deep neural network performing bnary classification
     """
     def __init__(self, nx, layers):
         """Class constructor"""
@@ -17,27 +17,29 @@ class DeepNeuralNetwork:
             raise ValueError("nx must be a positive integer")
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-        for i in layers:
-            if not isinstance(i, int) or i <= 0:
-                raise TypeError("layers must be a list of positive integers")
+        if not all(isinstance(i, int) and i > 0 for i in layers):
+            raise TypeError("layers must be a list of positive integers")
+
         self.__L = len(layers)
         self.__cache = {}
         self.__weights = {}
+
         for l in range(1, self.__L + 1):
             if l == 1:
-                self.__weights['W1'] = np.random.randn(layers[0], nx) * np.sqrt(2/nx)
+                self.__weights['W' + str(l)] = np.random.randn(layers[0], nx) * np.sqrt(2 / nx)
             else:
-                self.__weights['W{}'.format(l)] = np.random.randn(layers[l-1], layers[l-2]) * np.sqrt(2/layers[l-2])
-            self.__weights['b{}'.format(l)] = np.zeros((layers[l-1], 1))
-    
+                self.__weights['W' + str(l)] = np.random.randn(layers[l - 1], layers[l - 2]) * np.sqrt(2 / layers[l - 2])
+            
+            self.__weights['b' + str(l)] = np.zeros((layers[l - 1], 1))
+
     @property
     def L(self):
         return self.__L
-    
+
     @property
     def cache(self):
         return self.__cache
-    
+
     @property
     def weights(self):
         return self.__weights
