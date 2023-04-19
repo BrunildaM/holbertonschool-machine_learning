a21cf78998ffaf24ce12d4cc95399338a1cd3da6
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-04-19 19:50:52 +0200

    Update 24-one_hot_encode.py

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -14,5 +14,5 @@ def one_hot_encode(Y, classes):
         one_hot = np.zeros((classes, m))
         one_hot[Y, np.arange(m)] = 1
         return one_hot
-    except:
+    except Exception:
         return None
