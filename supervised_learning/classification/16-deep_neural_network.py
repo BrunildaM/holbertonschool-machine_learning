8aff9411b7e0042a057da4277bc7ef691450b5bb
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-04-19 20:01:50 +0200

    Update 16-deep_neural_network.py

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -17,18 +17,29 @@ class DeepNeuralNetwork:
             raise ValueError("nx must be a positive integer")
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-        if not all(isinstance(layer, int) and layer > 0 for layer in layers):
-            raise TypeError("layers must be a list of positive integers")
+        for i in layers:
+            if not isinstance(i, int) or i < 1:
+                raise TypeError("layers must be a list of positive integers")
 
         self.__L = len(layers)
         self.__cache = {}
         self.__weights = {}
 
-        for l in range(1, self.__L + 1):
-            if l == 1:
-                w = np.random.randn(layers[l-1], nx) * np.sqrt(2/nx)
+        for i in range(self.L):
+            if i == 0:
+                self.__weights["W1"] = np.random.randn(layers[i], nx) * np.sqrt(2 / nx)
             else:
-                w = np.random.randn(layers[l-1], layers[l-2]) * np.sqrt(2/layers[l-2])
-            b = np.zeros((layers[l-1], 1))
-            self.__weights['W' + str(l)] = w
-            self.__weights['b' + str(l)] = b
+                self.__weights["W{}".format(i+1)] = np.random.randn(layers[i], layers[i-1]) * np.sqrt(2 / layers[i-1])
+            self.__weights["b{}".format(i+1)] = np.zeros((layers[i], 1))
+
+    @property
+    def L(self):
+        return self.__L
+
+    @property
+    def cache(self):
+        return self.__cache
+
+    @property
+    def weights(self):
+        return self.__weights
