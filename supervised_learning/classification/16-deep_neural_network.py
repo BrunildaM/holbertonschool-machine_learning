099c66f77cc525c0fcca95ffb4813523684d09b4
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-04-19 20:05:43 +0200

    Update 16-deep_neural_network.py

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -11,35 +11,20 @@ class DeepNeuralNetwork:
     """
     def __init__(self, nx, layers):
         """class constructor"""
-        if not isinstance(nx, int):
+        if type(nx) != int:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if not isinstance(layers, list) or len(layers) == 0:
+        if type(layers) != list or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-        for i in layers:
-            if not isinstance(i, int) or i < 1:
+        for i, n in enumerate(layers):
+            if type(n) != int or n < 1:
                 raise TypeError("layers must be a list of positive integers")
-
-        self.__L = len(layers)
-        self.__cache = {}
-        self.__weights = {}
-
-        for i in range(self.L):
             if i == 0:
-                self.__weights["W1"] = np.random.randn(layers[i], nx) * np.sqrt(2 / nx)
+                self.weights = {"W1": np.random.randn(n, nx) * np.sqrt(2 / nx),
+                                "b1": np.zeros((n, 1))}
             else:
-                self.__weights["W{}".format(i+1)] = np.random.randn(layers[i], layers[i-1]) * np.sqrt(2 / layers[i-1])
-            self.__weights["b{}".format(i+1)] = np.zeros((layers[i], 1))
-
-    @property
-    def L(self):
-        return self.__L
-
-    @property
-    def cache(self):
-        return self.__cache
-
-    @property
-    def weights(self):
-        return self.__weights
+                self.weights[f"W{i + 1}"] = np.random.randn(n, layers[i - 1]) * np.sqrt(2 / layers[i - 1])
+                self.weights[f"b{i + 1}"] = np.zeros((n, 1))
+        self.L = len(layers)
+        self.cache = {}
