91aad36b6c0050f83e3d0e992d7bf026c9459ed1
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-04-19 19:43:42 +0200

    Update 24-one_hot_encode.py

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -4,11 +4,12 @@ import numpy as np
 
 
 def one_hot_encode(Y, classes):
+    """a function that converts a one-hot matrix into a vector of labels"""
     try:
         assert isinstance(Y, np.ndarray)
         assert isinstance(classes, int)
         assert len(Y.shape) == 1
-        
+
         m = len(Y)
         one_hot = np.zeros((classes, m))
         one_hot[Y, np.arange(m)] = 1
@@ -16,3 +17,10 @@ def one_hot_encode(Y, classes):
     except Exception as e:
         print("Error in one_hot_encode:", str(e))
         return None
+
+Y = [0, 1, 2, 1, 0]
+Y = np.array(Y)
+classes = 3
+
+one_hot = one_hot_encode(Y, classes)
+print(one_hot)
