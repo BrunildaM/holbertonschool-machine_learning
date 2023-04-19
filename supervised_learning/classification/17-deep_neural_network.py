05bfac4bbf2dda38829a4572fae8d8f4c8c8731a
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-04-19 20:43:20 +0200

    Update 17-deep_neural_network.py

--- supervised_learning/classification/17-deep_neural_network.py
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -6,34 +6,43 @@ import numpy as np
 
 
 class DeepNeuralNetwork:
+    """
+    class that represents a deep neural network
+    performing binary classification
+    """
     def __init__(self, nx, layers):
-        if not isinstance(nx, int):
+        """class constructor"""
+        if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if not isinstance(layers, list) or len(layers) == 0:
+        if type(layers) is not list or len(layers) < 1:
             raise TypeError("layers must be a list of positive integers")
-        for n in layers:
-            if not isinstance(n, int) or n < 1:
+            
+        weights = {}
+        previous = nx
+        
+        for index, layer in enumerate(layers, 1):
+            
+            if type(layer) is not int or layer < 0:
                 raise TypeError("layers must be a list of positive integers")
+            
+            weights["b{}".format(index)] = np.zeros((layer, 1))
+            weights["W{}".format(index)] = (np.random.randn(layer, previous) * np.sqrt(2 / previous))
+            previous = layer
+            
         self.__L = len(layers)
         self.__cache = {}
-        self.__weights = {}
-        for i in range(1, self.__L + 1):
-            if i == 1:
-                self.__weights['W' + str(i)] = np.random.randn(layers[i-1], nx) * np.sqrt(2/nx)
-            else:
-                self.__weights['W' + str(i)] = np.random.randn(layers[i-1], layers[i-2]) * np.sqrt(2/layers[i-2])
-            self.__weights['b' + str(i)] = np.zeros((layers[i-1], 1))
-
+        self.__weights = weights
+        
     @property
     def L(self):
-        return self.__L
+        return (self.__L)
 
     @property
     def cache(self):
-        return self.__cache
+        return (self.__cache)
 
     @property
     def weights(self):
-        return self.__weights
+        return (self.__weights)
