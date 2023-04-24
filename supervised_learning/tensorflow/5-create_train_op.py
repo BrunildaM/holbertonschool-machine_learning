2cfff68eb36cb2fec6fe62243929265dfe9af702
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-04-24 19:08:04 +0200

    Create 5-create_train_op.py

---
+++ supervised_learning/tensorflow/5-create_train_op.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+a function that creates the training operation for the network
+"""
+import tensorflow as tf
+
+
+def create_train_op(loss, alpha):
+    """
+    Creates the training operation for the network using gradient descent.
+
+    Args:
+        loss (tf.Tensor): the loss of the network's prediction.
+        alpha (float): the learning rate.
+
+    Returns:
+        tf.Operation: an operation that trains the network
+        using gradient descent.
+    """
+    optimizer = tf.train.GradientDescentOptimizer(alpha)
+    train_op = optimizer.minimize(loss)
+    return train_op
