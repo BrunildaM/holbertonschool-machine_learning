2c54a2eef65e62195053be077a8dc552e6f0e8a3
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-04-24 19:43:48 +0200

    Update 6-train.py

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -4,7 +4,6 @@ A function  that builds, trains, and saves a neural network classifier
 """
 import tensorflow as tf
 
-
 calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
 calculate_loss = __import__('4-calculate_loss').calculate_loss
 create_placeholders = __import__('0-create_placeholders').create_placeholders
@@ -12,23 +11,15 @@ create_train_op = __import__('5-create_train_op').create_train_op
 forward_prop = __import__('2-forward_prop').forward_prop
 
 
-def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
-          alpha, iterations, save_path="/tmp/model.ckpt"):
-    """
-    A function  that builds, trains, and saves a neural network classifier
-    """
-    tf.set_random_seed(0)
-    np.random.seed(0)
+def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, iterations, save_path="/tmp/model.ckpt"):
+    """Builds, trains, and saves a neural network classifier"""
+    nx = X_train.shape[1]
+    classes = Y_train.shape[1]
 
-    input_size = X_train.shape[1]
-    output_size = Y_train.shape[1]
-
-    x, y = create_placeholders(input_size, output_size)
+    x, y = create_placeholders(nx, classes)
 
     y_pred = forward_prop(x, layer_sizes, activations)
-
     loss = calculate_loss(y, y_pred)
-
     accuracy = calculate_accuracy(y, y_pred)
 
     train_op = create_train_op(loss, alpha)
@@ -40,27 +31,25 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
     tf.add_to_collection('accuracy', accuracy)
     tf.add_to_collection('train_op', train_op)
 
-    sess = tf.Session()
-    init_op = tf.global_variables_initializer()
-    sess.run(init_op)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
 
-    for i in range(iterations + 1):
-        _, cost = sess.run([train_op, loss], feed_dict={x: X_train, y: Y_train})
+        for i in range(iterations+1):
+            train_cost, train_acc = sess.run([loss, accuracy], feed_dict={x: X_train, y: Y_train})
+            valid_cost, valid_acc = sess.run([loss, accuracy], feed_dict={x: X_valid, y: Y_valid})
 
-        if i % 100 == 0:
-            train_acc, train_cost = sess.run([accuracy, loss],
-                                             feed_dict={x: X_train, y: Y_train})
-            valid_acc, valid_cost = sess.run([accuracy, loss],
-                                             feed_dict={x: X_valid, y: Y_valid})
+            if i % 100 == 0:
+                print("After {} iterations:".format(i))
+                print("\tTraining Cost: {}".format(train_cost))
+                print("\tTraining Accuracy: {}".format(train_acc))
+                print("\tValidation Cost: {}".format(valid_cost))
+                print("\tValidation Accuracy: {}".format(valid_acc))
 
-            print("After {} iterations:".format(i))
-            print("\tTraining Cost: {}".format(train_cost))
-            print("\tTraining Accuracy: {}".format(train_acc))
-            print("\tValidation Cost: {}".format(valid_cost))
-            print("\tValidation Accuracy: {}".format(valid_acc))
+            sess.run(train_op, feed_dict={x: X_train, y: Y_train})
 
-    saver = tf.train.Saver()
-    save_path = saver.save(sess, save_path)
-    sess.close()
+        saver = tf.train.Saver()
+        save_path = saver.save(sess, save_path)
+
+    print("Model saved in path: {}".format(save_path))
 
     return save_path
