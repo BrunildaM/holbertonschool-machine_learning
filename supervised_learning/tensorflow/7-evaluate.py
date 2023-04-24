eba8e109f3294b4ba931cec092bc60ee58fd816c
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-04-24 19:30:49 +0200

    Create 7-evaluate.py

---
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+A function that evaluates the output of a neural network
+"""
+import tensorflow as tf
+
+
+def evaluate(X, Y, save_path):
+    """
+    A function that evaluates the outout of a neural network
+    """
+    with tf.Session() as sess:
+        loader = tf.train.import_meta_graph(save_path + '.meta')
+        loader.restore(sess, save_path)
+
+        graph = tf.get_default_graph()
+        x = graph.get_tensor_by_name('x:0')
+        y = graph.get_tensor_by_name('y:0')
+        y_pred = graph.get_tensor_by_name('y_pred:0')
+        loss = graph.get_tensor_by_name('loss:0')
+        accuracy = graph.get_tensor_by_name('accuracy:0')
+
+        y_pred, loss, accuracy = sess.run([y_pred, loss, accuracy], feed_dict={x: X, y: Y})
+
+    return y_pred, accuracy, loss
