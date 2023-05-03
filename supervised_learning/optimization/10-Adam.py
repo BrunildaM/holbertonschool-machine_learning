cba4aa2c7ea2464a9a04a50439c60dee85e49a8c
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-05-03 15:33:25 +0200

    Create 10-Adam.py

---
+++ supervised_learning/optimization/10-Adam.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+A function that creates the training operation for a neural network
+in tensorflow using the Adam optimization algorithm
+"""
+import tensorflow as tf
+
+
+def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
+    """
+    A function that creates the training operation for a neural network
+    in tensorflow using the Adam optimization algorithm
+    """
+    optimizer = tf.keras.optimizers.Adam(
+        learning_rate=alpha, 
+        beta_1=beta1, 
+        beta_2=beta2, 
+        epsilon=epsilon)
+    return optimizer.minimize(loss)
