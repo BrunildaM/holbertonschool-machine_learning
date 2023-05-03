aeb6562124731f015e7c5de4305801b4d0c677fb
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-05-03 15:22:29 +0200

    Update 6-momentum.py

--- supervised_learning/optimization/6-momentum.py
+++ supervised_learning/optimization/6-momentum.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """
-A function that creates the training operation for a neural network in tensorflow
-using the gradient descent with momentum optimization algorithm"""
+A function that creates the training operation for a neural network in
+tensorflow using the gradient descent with momentum optimization algorithm
+"""
 import tensorflow as tf
 
 
