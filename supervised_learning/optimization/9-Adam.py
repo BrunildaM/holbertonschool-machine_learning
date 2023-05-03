469afa3c467a0070f3ce15857dc8a8d276784ca5
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-05-03 15:31:40 +0200

    Update 9-Adam.py

--- supervised_learning/optimization/9-Adam.py
+++ supervised_learning/optimization/9-Adam.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 """
-A function that updates a variable in place using the Adam optimization algorithm
+A function that updates a variable in place
+using the Adam optimization algorithm
 """
 
 
 def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
     """
-    A function that updates a variable in place using the Adam optimization algorithm
+    A function that updates a variable in place
+    using the Adam optimization algorithm
     """
     v_dW = (beta1 * v) + ((1 - beta1) * grad)
     s_dW = (beta2 * s) + ((1 - beta2) * (grad ** 2))
