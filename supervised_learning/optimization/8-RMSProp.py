d1ef90376d820c096c49a18f14b12d39879a3ccc
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-05-03 15:27:15 +0200

    Create 8-RMSProp.py

---
+++ supervised_learning/optimization/8-RMSProp.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+A function that creates the training operation for a neural network
+in tensorflow using the RMSProp optimization algorithm
+"""
+
+
+def create_RMSProp_op(loss, alpha, beta2, epsilon):
+    """
+    A function that creates the training operation for a neural network
+    in tensorflow using the RMSProp optimization algorithm"""
+    optimizer = tf.train.RMSPropOptimizer(learning_rate=alpha, decay=beta2, epsilon=epsilon)
+    train_op = optimizer.minimize(loss)
+    return train_op
