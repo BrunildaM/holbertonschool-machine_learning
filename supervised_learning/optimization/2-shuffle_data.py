9f8ed2f7549e5261d0d40c29cabf76b1281d777d
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-05-03 13:16:32 +0200

    Update 2-shuffle_data.py

--- supervised_learning/optimization/2-shuffle_data.py
+++ supervised_learning/optimization/2-shuffle_data.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """A function that shuffles the data points
 in two matrices the same way"""
+import numpy as np
+
 
 def shuffle_data(X, Y):
     """A function that shuffles the data points
