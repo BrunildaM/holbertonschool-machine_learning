28510bebcf5fc2e9265318e1057ae4a7561f4d10
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-05-03 15:56:36 +0200

    Update 10-Adam.py

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -7,13 +7,8 @@ import tensorflow as tf
 
 
 def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
-    """
-    A function that creates the training operation for a neural network
-    in tensorflow using the Adam optimization algorithm
-    """
-    optimizer = tf.keras.optimizers.Adam(
-        learning_rate=alpha,
-        beta_1=beta1,
-        beta_2=beta2,
-        epsilon=epsilon)
-    return optimizer.minimize(loss)
+    """A function that creates the training operation for a neural
+    network in tensorflow using the Adam optimization algorithm"""
+    optimizer = tf.train.AdamOptimizer(alpha, beta1=beta1, beta2=beta2,
+                                       epsilon=epsilon)
+    return (optimizer.minimize(loss))
