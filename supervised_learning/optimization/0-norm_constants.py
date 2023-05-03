17ecd4538ed982cdb59e015883a49d0bcf5feba4
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-05-03 13:11:39 +0200

    Update 0-norm_constants.py

--- supervised_learning/optimization/0-norm_constants.py
+++ supervised_learning/optimization/0-norm_constants.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
-import numpy as np
 """Function that calculates the mean and standard deviation"""
+import numpy as np
 
 
 def normalization_constants(X):
     """
-    Calculates the mean and standard deviation of each feature in the input matrix X.
+    Calculates the mean and standard deviation of
+    each feature in the input matrix X.
     """
     mean = np.mean(X, axis=0)
     std = np.std(X, axis=0)
-    
+
     return mean, std
