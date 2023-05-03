b34bf2ffbcd07ebfb5981a8afd7f32956b4caf82
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-05-03 15:27:54 +0200

    Update 8-RMSProp.py

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -3,6 +3,7 @@
 A function that creates the training operation for a neural network
 in tensorflow using the RMSProp optimization algorithm
 """
+import tensorflow as tf
 
 
 def create_RMSProp_op(loss, alpha, beta2, epsilon):
