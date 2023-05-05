77f46104a1fe30e9f6d4531b5bffb3af5497525d
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-05-05 16:34:27 +0200

    Create 4-f1_score.py

---
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""A function that calculates the F1 score of a confusion matrix"""
+import numpy as np
+sensitivity = __import__('1-sensitivity').sensitivity
+precision = __import__('2-precision').precision
+
+
+def f1_score(confusion: np.ndarray) -> List[float]:
+    """A function that calculates the F1 score of a confusion
+    matrix"""
+    classes = confusion.shape[0]
+    f1_scores = np.zeros(classes)
+    for i in range(classes):
+        p = precision(confusion)[i]
+        r = sensitivity(confusion)[i]
+        if p + r != 0:
+            f1_scores[i] = 2 * (p * r) / (p + r)
+    return f1_scores
