c9d45924f129a514db2d42691c2b7dd0bd47b369
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-05-05 16:40:45 +0200

    Update 4-f1_score.py

--- supervised_learning/error_analysis/4-f1_score.py
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -8,14 +8,7 @@ precision = __import__('2-precision').precision
 def f1_score(confusion):
     """A function that calculates the F1 score of a confusion
     matrix"""
-    classes = confusion.shape[0]
-    f1_scores = []
-    for i in range(classes):
-        tp = confusion[i][i]
-        fp = sum(confusion[:, i]) - tp
-        fn = sum(confusion[i, :]) - tp
-        p = precision(confusion)[i]
-        r = sensitivity(confusion)[i]
-        if p + r != 0:
-            f1_scores[i] = 2 * (p * r) / (p + r)
-    return f1_scores
+    precisions = precision(confusion)
+    sensitivities = sensitivity(confusion)
+    return 2 * (precisions * sensitivities) / (precisions
+                                               + sensitivities)
