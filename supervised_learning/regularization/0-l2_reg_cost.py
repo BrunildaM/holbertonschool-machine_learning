dc8b81e4d40a8667cc3699b5f3fe8ae7b7072096
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-05-10 16:16:48 +0200

    Update 0-l2_reg_cost.py

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -11,6 +11,6 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     """
     l2_norm_squared = 0
     for l in range(1, L+1):
-        l2_norm_squared += np.sum(np.square(weights[f"W{l}"]))
+        l2_norm_squared += np.sum(np.square(weights["W" + str(l)]))
     l2_regularization_cost = (lambtha / (2 * m)) * l2_norm_squared
     return cost + l2_regularization_cost
