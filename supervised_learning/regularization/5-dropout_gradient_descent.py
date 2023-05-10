1b8ad4b8e20c6cd4911f900b2b0fc74f6a2b3827
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-05-10 22:07:49 +0200

    Update 5-dropout_gradient_descent.py

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -12,27 +12,27 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     regularization using gradient descent
     """
     m = Y.shape[1]
-    dZ = cache['A'+str(L)] - Y
-    
+    dZ = cache['A' + str(L)] - Y
+
     for l in reversed(range(1, L+1)):
-        A_prev = cache['A'+str(l-1)]
-        A = cache['A'+str(l)]
-        W = weights['W'+str(l)]
-        b = weights['b'+str(l)]
+        A_prev = cache['A' + str(l-1)]
+        A = cache['A' + str(l)]
+        W = weights['W' + str(l)]
+        b = weights['b' + str(l)]
 
         if l > 1:
-            D = cache['D'+str(l-1)]
+            D = cache['D' + str(l-1)]
             dA = np.dot(W.T, dZ) * D / keep_prob
         else:
             dA = np.dot(W.T, dZ)
-        
+
         dW = np.dot(dZ, A_prev.T) / m
         db = np.sum(dZ, axis=1, keepdims=True) / m
-        
+
         W -= alpha * dW
         b -= alpha * db
-        
-        dZ = dA * (1 - np.power(A, 2)) # tanh derivative
-    
-    weights['W'+str(l)] = W
-    weights['b'+str(l)] = b
+
+        dZ = dA * (1 - np.power(A, 2))
+
+    weights['W' + str(l)] = W
+    weights['b' + str(l)] = b
