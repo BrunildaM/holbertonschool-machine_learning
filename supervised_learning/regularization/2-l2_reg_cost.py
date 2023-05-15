52aeb12041117b8bf76c3905c473cd90f56c83df
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-05-15 09:50:49 +0200

    Update 2-l2_reg_cost.py

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -8,7 +8,4 @@ def l2_reg_cost(cost):
     """
     A function that calculates the cost of a neural network
     with l2 regularization"""
-    graph = tf.get_default_graph()
-    losses = graph.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
-    cost_reg = tf.add(cost, tf.reduce_sum(losses))
-    return cost_reg
+    return cost + tf.losses.get_regularization_losses()
