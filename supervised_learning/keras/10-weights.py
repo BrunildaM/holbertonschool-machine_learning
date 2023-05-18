f5238ff76322be53b653023e06d6171c0b820053
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-05-18 23:36:49 +0200

    Update 10-weights.py

--- supervised_learning/keras/10-weights.py
+++ supervised_learning/keras/10-weights.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
 """A function that saves a model's weights"""
 import tensorflow.keras as K
-one_hot = __import__('3-one_hot').one_hot
-train_model = __import__('8-train').train_model
-model = __import__('9-model')
-weights = __import__('10-weights')
 
 
 def save_weights(network, filename, save_format='h5'):
-   """A function that saves a model's weights"""
+    """A function that saves a model's weights"""
     network.save_weights(filename, save_format=save_format)
     return None
 
