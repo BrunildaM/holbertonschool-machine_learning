9f2037f766bafdb9b735c6fca21ce047b02fc33e
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-05-18 23:20:03 +0200

    Create 9-model.py

---
+++ supervised_learning/keras/9-model.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""A function that saves an entire model"""
+import tensorflow.keras as K
+
+
+def save_model(network, filename):
+    """A function that saves an entire model"""
+    network.save(filename)
+    return None
+
+
+def load_model(filename):
+    model = K.models.load_model(filename)
+    return model
