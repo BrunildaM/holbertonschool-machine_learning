a74063292ba681ee73692bf7e6732b41616ffa06
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-05-18 11:52:10 +0200

    Update 0-sequential.py

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 """A function that builds a neural network with the Keras library"""
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout
-from tensorflow.keras import regularizers
+import tensorflow.keras as keras
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """A function that builds a neural network with the Keras library"""
-    model = Sequential()
-    model.add(Dense(layers[0], activation=activations[0], input_shape=(nx,),
-                    kernel_regularizer=regularizers.l2(lambtha)))
-    model.add(Dropout(1 - keep_prob))
+    model = keras.models.Sequential()
+    model.add(keras.layers.Dense(layers[0], activation=activations[0],
+                                 input_shape=(nx,),
+                    kernel_regularizer=keras.regularizers.l2(lambtha)))
+    model.add(keras.layers.Dropout(1 - keep_prob))
+
     for i in range(1, len(layers)):
-        model.add(Dense(layers[i], activation=activations[i],
-                        kernel_regularizer=regularizers.l2(lambtha)))
-        model.add(Dropout(1 - keep_prob))
+        model.add(keras.layers.Dense(layers[i], activation=activations[i],
+                        kernel_regularizer=keras.regularizers.l2(lambtha)))
+        model.add(keras.layers.Dropout(1 - keep_prob))
 
     return model
