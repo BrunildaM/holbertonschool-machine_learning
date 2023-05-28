3b712e25c7bb88f099f7a0344bb214ec7a3e9689
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-05-28 11:09:15 +0200

    Update 4-lenet5.py

--- supervised_learning/cnn/4-lenet5.py
+++ supervised_learning/cnn/4-lenet5.py
@@ -9,22 +9,22 @@ def lenet5(x, y):
     LeNet-5 architecture using tensorflow"""
     weights_initializer = tf.contrib.layers.variance_scaling_initializer()
     C1 = tf.layers.Conv2D(filters=6,
-                          kernel_size=(5,5),
+                          kernel_size=(5, 5),
                           padding='same',
                           activation=tf.nn.relu,
                           kernel_initializer=weights_initializer)
     output_1 = C1(x)
-    P2 = tf.layers.MaxPooling2D(pool_size=(2,2),
-                                strides=(2,2))
+    P2 = tf.layers.MaxPooling2D(pool_size=(2, 2),
+                                strides=(2, 2))
     output_2 = P2(output_1)
     C3 = tf.layers.Conv2D(filters=16,
-                          kerenl_size=(5,5),
+                          kernel_size=(5, 5),
                           padding='valid',
                           activation=tf.nn.relu,
                           kernel_initializer=weights_initializer)
     output_3 = C3(output_2)
-    P4 = tf.layers.MaxPooling2D(pool_size=(2,2),
-                                strides=(2,2))
+    P4 = tf.layers.MaxPooling2D(pool_size=(2, 2),
+                                strides=(2, 2))
     output_4 = P4(output_3)
     output_42 = tf.layers.Flatten()(output_4)
     F5 = tf.layers.Dense(
@@ -32,9 +32,10 @@ def lenet5(x, y):
         activation=tf.nn.relu,
         kernel_initializer=weights_initializer)
     output_5 = F5(output_42)
-    F6 = tf.layers.Dense(84,
-                         activation=tf.nn.relu,
-                         kernel_initializer=weights_initializer)
+    F6 = tf.layers.Dense(
+        84,
+        activation=tf.nn.relu,
+        kernel_initializer=weights_initializer)
     output_6 = F6(output_5)
     F7 = tf.layers.Dense(
         10,
