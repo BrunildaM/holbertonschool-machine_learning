2c24a9f39662fb2b3d2e0cf647e0391e9a9beb5c
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-07-06 23:33:57 +0200

    Update 8-bi_rnn.py

--- supervised_learning/RNNs/8-bi_rnn.py
+++ supervised_learning/RNNs/8-bi_rnn.py
@@ -1,39 +1,46 @@
 #!/usr/bin/env python3
 """
-A function that performs forward propagation
-for a bidirectional RNN
+Performs forward propagation for a bidirectional RNN
 """
 import numpy as np
 
 
 def bi_rnn(bi_cell, X, h_0, h_t):
     """
-    A function that performs forward propagation
-    for a bidirectional RNN
-    parameters:
-    bi_cell is an instance of BidirectionalCell
-    X is the data to be used
-      t is the max number of time steps
-      m is the batch size
-      i is the dimensionality of the data
-    h_0 is the initial hidden state in the forward direction
-      h is the dimensionality of the hidden state
-    h_t is the initial hidden state in the backward direction
+    Performs forward propagation for a bidirectional RNN
+    :param bi_cell: is an instance of BidirectinalCell that will be used for
+    the forward propagation
+    :param X: the data to be used, given as a numpy.ndarray of
+    shape (t, m, i)
+        t is the maximum number of time steps
+        m is the batch size
+        i is the dimensionality of the data
+    :param h_0: the initial hidden state, given as a numpy.ndarray of
+    shape (m, h)
+        h is the dimensionality of the hidden state
+    :param h_t: is the initial hidden state in the backward direction, given
+    as a numpy.ndarray of shape (m, h)
+    :return: H, Y
+        H is a numpy.ndarray containing all of the hidden states
+        Y is a numpy.ndarray containing all of the outputs
     """
     t, m, i = X.shape
     h = h_0.shape[1]
+    H_for = np.zeros((t, m, h))
+    H_back = np.zeros((t, m, h))
+    h_ft = h_0
+    h_bt = h_t
+    for step in range(t):
+        x_ft = X[step]
+        x_bt = X[-(step + 1)]
 
-    H_forward = np.zeros((t, m, h))
-    H_backward = np.zeros((t, m, h))
+        h_ft = bi_cell.forward(h_ft, x_ft)
+        h_bt = bi_cell.backward(h_bt, x_bt)
 
-    H_forward[0] = h_0
-    H_backward[t - 1] = h_t
+        H_for[step] = h_ft
+        H_back[-(step + 1)] = h_bt
 
-    for step in range(t - 1):
-        H_forward[step + 1], _ = bi_cell.forward(H_forward[step], X[step])
-        H_backward[t - step - 2], _ = bi_cell.backward(H_backward[t - step - 1], X[t - step - 1])
-
-    H = np.concatenate((H_forward[:-1], H_backward[1:]), axis=2)
+    H = np.concatenate((H_for, H_back), axis=-1)
     Y = bi_cell.output(H)
 
     return H, Y
