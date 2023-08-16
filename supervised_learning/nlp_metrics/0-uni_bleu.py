28dded202ecddb9983eac673403979eeca197299
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-08-16 09:21:17 +0200

    Update 0-uni_bleu.py

--- supervised_learning/nlp_metrics/0-uni_bleu.py
+++ supervised_learning/nlp_metrics/0-uni_bleu.py
@@ -11,11 +11,14 @@ def uni_bleu(references, sentence):
     for reference in references:
         reference_counts.update(reference)
 
-    clip_count = sum(min(candidate_counts[ngram], reference_counts[ngram]) for ngram in candidate_counts)
+    clip_count = sum(min(candidate_counts[ngram],
+                         reference_counts[ngram])
+                     for ngram in candidate_counts)
     candidate_total = sum(candidate_counts.values())
     reference_total = sum(reference_counts.values())
 
     precision = clip_count / candidate_total if candidate_total > 0 else 0
+
     brevity_penalty = min(1, len(sentence) / reference_total)
 
     bleu_score = precision * brevity_penalty
