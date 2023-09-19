8e20ad9c0d2d2fa8583b5fb091c29141406c0410
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-09-19 12:17:42 +0200

    Update 2-variance.py

--- unsupervised_learning/clustering/2-variance.py
+++ unsupervised_learning/clustering/2-variance.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
-"""A function that calculates the total intra-cluster variance
-for a data set"""
+"""
+Calculates the total intra-cluster variance for a data set
+"""
 import numpy as np
 
 
 def variance(X, C):
-    """A funtion that calcultes the total intra-cluster
-    variance for a data set
-    X: is a numpy.ndarray of shape (n, d) containing the data set
-    C: is a numpy.ndarray of shape (k, d) containing the
-    centroid means for each cluster
-    returns: var or None on failure
-    var is the total variance
+    """
+    Calculates the intra-cluster variance
+    X: numpy.ndarray of shape (n, d) containing the data set
+    C: numpy.ndarray of shape (k, d) containing the centroid means for
+    each cluster
+    return: var, or None on failure
+        var is the total variance
     """
     if type(X) is not np.ndarray or len(X.shape) != 2:
         return None
-    if type(C) is not np.ndarray or len(X.shape) != 2:
+    if type(C) is not np.ndarray or len(C.shape) != 2:
         return None
     k, d = C.shape
     if type(k) is not int or k <= 0:
@@ -24,5 +25,6 @@ def variance(X, C):
         return None
     D = np.sqrt(((X - C[:, np.newaxis]) ** 2).sum(axis=2))
     cluster = np.min(D, axis=0)
+
     var = np.sum(np.square(cluster))
     return var
