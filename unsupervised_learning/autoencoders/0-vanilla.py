5a62f9f5bba69c56b7dbe517bf9f85e0b6731bd4
BrunildaM <105221208+BrunildaM@users.noreply.github.com>
2023-10-05 18:03:23 +0200

    Update 0-vanilla.py

--- unsupervised_learning/autoencoders/0-vanilla.py
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -14,22 +14,25 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     latent_dims: an integer containing the dimensions of the latent
     space representation
     return: encoder, decoder, auto
+        encoder is the encoder model
+        decoder is the decoder model
+        auto is the full autoencoder model
     """
     input_encoder = keras.Input(shape=(input_dims, ))
     input_decoder = keras.Input(shape=(latent_dims, ))
 
     encoded = keras.layers.Dense(hidden_layers[0],
-                                activation='relu')(input_encoder)
+                                 activation='relu')(input_encoder)
     for enc in range(1, len(hidden_layers)):
         encoded = keras.layers.Dense(hidden_layers[enc],
-                                    activation='relu')(encoded)
+                                     activation='relu')(encoded)
 
-    latent = keras.layers.Dense(latent_dims, activation= 'relu')(encoded)
+    latent = keras.layers.Dense(latent_dims, activation='relu')(encoded)
     encoder = keras.Model(inputs=input_encoder, outputs=latent)
 
-    decoded = keras.layers.Dense(hidden_layers[dec],
-                                 activation='relu')(decoded)
-    for dec in range(len(hidden_layers) -2, -1, -1):
+    decoded = keras.layers.Dense(hidden_layers[-1],
+                                 activation='relu')(input_decoder)
+    for dec in range(len(hidden_layers) - 2, -1, -1):
         decoded = keras.layers.Dense(hidden_layers[dec],
                                      activation='relu')(decoded)
     last = keras.layers.Dense(input_dims, activation='sigmoid')(decoded)
